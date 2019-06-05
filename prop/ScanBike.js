7daa247e3df2bbd8626d7e24b748e0593ec8ddf6
Josh Steubs <joshua.steubs@gmail.com>
2019-06-05 04:34:19 -0700

    fix camera scan add readme

--- prop/ScanBike.js
+++ prop/ScanBike.js
@@ -1,59 +1,140 @@
 import React, { Component } from 'react';
+import { Button, Text, View } from 'react-native';
+import { RNCamera } from 'react-native-camera';
 
-import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    Linking,
-} from 'react-native';
+class ProductScanRNCamera extends Component {
 
-import QRCodeScanner from 'react-native-qrcode-scanner';
+    constructor(props) {
+        super(props);
+        this.camera = null;
+        this.barcodeCodes = [];
 
-class ScanScreen extends Component {
-    onSuccess = (e) => {
-        Linking
-            .openURL(e.data)
-            .catch(err => console.error('An error occured', err));
+        this.state = {
+            camera: {
+                type: RNCamera.Constants.Type.back,
+                flashMode: RNCamera.Constants.FlashMode.auto,
+                barcodeFinderVisible: true
+            }
+        };
+    }
+
+    onBarCodeRead(scanResult) {
+        console.warn(scanResult.type);
+        console.warn(scanResult.data);
+        if (scanResult.data != null) {
+            if (!this.barcodeCodes.includes(scanResult.data)) {
+                this.barcodeCodes.push(scanResult.data);
+                console.warn('onBarCodeRead call');
+            }
+        }
+        return;
+    }
+
+    async takePicture() {
+        if (this.camera) {
+            const options = { quality: 0.5, base64: true };
+            const data = await this.camera.takePictureAsync(options);
+            console.log(data.uri);
+        }
+    }
+
+    pendingView() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    backgroundColor: 'lightgreen',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <Text>Waiting</Text>
+            </View>
+        );
     }
 
     render() {
         return (
-            <QRCodeScanner
-                onRead={this.onSuccess}
-                topContent={
-                    <Text style={styles.centerText}>
-                        Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
-          </Text>
-                }
-                bottomContent={
-                    <TouchableOpacity style={styles.buttonTouchable}>
-                        <Text style={styles.buttonText}>OK. Got it!</Text>
-                    </TouchableOpacity>
-                }
-            />
+            <View style={styles.container}>
+                <RNCamera
+                    ref={ref => {
+                        this.camera = ref;
+                    }}
+                    captureAudio={false}
+                    barcodeFinderVisible={this.state.camera.barcodeFinderVisible}
+                    barcodeFinderWidth={280}
+                    barcodeFinderHeight={220}
+                    barcodeFinderBorderColor="white"
+                    barcodeFinderBorderWidth={2}
+                    defaultTouchToFocus
+                    flashMode={this.state.camera.flashMode}
+                    mirrorImage={false}
+                    onBarCodeRead={this.onBarCodeRead.bind(this)}
+                    onFocusChanged={() => { }}
+                    onZoomChanged={() => { }}
+                    androidCameraPermissionOptions={{
+                        title: 'Permission to use camera',
+                        message: 'We need your permission to use your camera',
+                        buttonPositive: 'Ok',
+                        buttonNegative: 'Cancel',
+                    }}
+                    style={styles.preview}
+                    type={this.state.camera.type}
+                />
+                <View style={[styles.overlay, styles.bottomOverlay]}>
+                    <Button
+                        onPress={() => { console.log('scan clicked'); }}
+                        style={styles.enterBarcodeManualButton}
+                        title="Back"
+                    />
+                </View>
+            </View>
         );
     }
 }
 
-const styles = StyleSheet.create({
-    centerText: {
+const styles = {
+    container: {
+        flex: 1
+    },
+    preview: {
         flex: 1,
-        fontSize: 18,
-        padding: 32,
-        color: '#777',
+        justifyContent: 'flex-end',
+        alignItems: 'center'
     },
-    textBold: {
-        fontWeight: '500',
-        color: '#000',
-    },
-    buttonText: {
-        fontSize: 21,
-        color: 'rgb(0,122,255)',
-    },
-    buttonTouchable: {
+    overlay: {
+        position: 'absolute',
         padding: 16,
+        right: 0,
+        left: 0,
+        alignItems: 'center'
     },
-});
+    topOverlay: {
+        top: 0,
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    bottomOverlay: {
+        bottom: 0,
+        backgroundColor: 'rgba(0,0,0,0.4)',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    enterBarcodeManualButton: {
+        padding: 15,
+        backgroundColor: 'white',
+        borderRadius: 40
+    },
+    scanScreenMessage: {
+        fontSize: 14,
+        color: 'white',
+        textAlign: 'center',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+};
 
-AppRegistry.registerComponent('default', () => ScanScreen);
+export default ProductScanRNCamera;
