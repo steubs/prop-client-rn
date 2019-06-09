79f69cd4dc5715e9b690cdc6799a430723b12452
Josh Steubs <joshua.steubs@gmail.com>
2019-06-09 07:26:27 -0700

    push before reseting podfile

--- prop/ios/Pods/Target Support Files/React-5e21c612/React-5e21c612-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_React_5e21c612 : NSObject
-@end
-@implementation PodsDummy_React_5e21c612
-@end
---
+++ prop/ios/Pods/Target Support Files/React-f0982950/React-f0982950-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_React_f0982950 : NSObject
+@end
+@implementation PodsDummy_React_f0982950
+@end
